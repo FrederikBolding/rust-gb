50409859412aed363cfdae5de1e5691b9a998aea
Frederik Bolding <frederik.bolding@gmail.com>
2022-12-03 11:45:18 +0100

    Add basic frame buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate num;
 #[macro_use]
 extern crate num_derive;
+extern crate minifb;
 
 mod cpu;
 mod mmu;
@@ -9,10 +10,40 @@ mod registers;
 
 use cpu::CPU;
 use mmu::MMU;
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
 
 fn main() {
     let mmu = MMU::new();
     let mut cpu = CPU::new(mmu);
     cpu.step();
     println!("Foo bar");
+
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
 }
