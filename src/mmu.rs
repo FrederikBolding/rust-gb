1be9fb709c0980e745d529ebada51b8e35e2ec77
Frederik Bolding <frederik.bolding@gmail.com>
2023-01-15 00:47:10 +0100

    Fix DMA

--- src/mmu.rs
+++ src/mmu.rs
@@ -352,7 +352,7 @@ impl MMU {
                         // Direct Memory Access
                         let source = (value as u16) << 8;
                         let destination = OAM_START as u16;
-                        for offset in 0..150 {
+                        for offset in 0..160 {
                             let byte = self.read(source + offset);
                             self.write(destination + offset, byte)
                         }
